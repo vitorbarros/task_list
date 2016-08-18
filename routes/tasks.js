ab36a7574601e5ae42316213a844f1774af1bf21
Vitor Barros <vitormonteirodebarros@gmail.com>
2016-08-18 03:22:53 -0300

    finalizando projeto final

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,87 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./../models');
 
+//index
 router.get('/', function(req, res){
-    res.render('tasks');
+    db.Tasks.findAll()
+        .then(function(result){
+            res.render('tasks', {
+                tasks: result
+            });
+        })
+        .catch(function(err){
+            console.log('Error in findAll tasks => ', err);
+        });
 });
+//show
+router.get('/:id', function(req, res){
+    db.Tasks.findById(req.params.id)
+        .then(function(result){
+            res.render('task', {
+                task: result
+            });
+        })
+        .catch(function(err){
+            console.log('Error in find one task => ', err);
+        });
+});
+//store
+router.post('/', function(req, res){
+    if(!req.body.name || !req.body.name.length){
+        return res.redirect('/tasks/create');
+    }
+    db.Tasks.create(req.body)
+        .then(function(result){
+            return res.redirect('/tasks');
+        })
+        .catch(function(err){
+            console.log('Error While Creating a task => ', err);
+        });
+});
+//delete
+router.delete('/:id', function(req, res){
+    db.Tasks.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(function(){
+        return res.redirect('/tasks');
+    })
+    .catch(function(err){
+        console.log('Error while deleting a task => ', err);
+    });
+});
+//create
 router.get('/create', function(req, res){
     res.render('new_task');
+});
+//edit
+router.get('/edit/:id', function(req, res){
+    db.Tasks.findById(req.params.id)
+        .then(function(result){
+            res.render('edit_task', {
+                task: result
+            });
+        })
+        .catch(function(err){
+            console.log('Error while editing a task => ', err);
+        });
+});
+//update
+router.put('/:id', function(req, res){
+    db.Tasks.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(function(result){
+        return res.redirect('/tasks');
+    })
+    .catch(function(err){
+        console.log('Error while deleting a task => ', err);
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./../models');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -7,6 +8,25 @@ router.get('/', function(req, res) {
 });
 router.get('/auth', function(req, res) {
   res.render('login');
+});
+
+//login
+router.post('/auth', function(req, res) {
+    db.Users.findOne({
+        where: {
+            username: req.body.username,
+            password: req.body.password
+        }
+    })
+    .then(function(result){
+        if(!result){
+            return res.redirect('/users/auth');
+        }
+        return res.redirect('/tasks');
+    })
+    .catch(function(err){
+        console.log('Login error =>', err);
+    });
 });
 
 module.exports = router;
